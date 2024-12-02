3b965271ab282e23ed5d6dc2ca56a365e163860d
robertcerq23 <robertpaixaosoares@gmail.com>
2024-12-01 22:53:10 -0300

    Add files via upload

--- styles.ts
+++ styles.ts
@@ -1,48 +1,54 @@
 import { styled } from 'styled-components/native';
 
-export const Wrapper = styled.SafeAreaView`
-  flex: 1;
-  justify-content: flex-start;
-  align-items: center;
+export const Container = styled.TouchableOpacity`
+  background-color: ${({theme})=>theme.COLORS.WHITE};
+  border: 1px solid ${({theme})=>theme.COLORS.GRAY_02};
+  align-items: left;
+  gap: 4px;
+  width: 100%;
+  height: 100px;
+  border-radius: 16px;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-bottom: 16px;
 `;
 
-export const Header = styled.View`
-  flex-direction: row;
+export const Content = styled.View`
+  gap: 8px;
+  padding: 16px;
   justify-content: space-between;
-  width: 90%;
-  padding: 16px 0;
-  border-bottom-width: 1px;
-  border-bottom-style: solid;
-  border-bottom-color: ${({theme})=>theme.COLORS.GREEN};
+  flex-direction: column;
+  max-width: 80%;
 `;
-export const HeaderButtonContainer = styled.TouchableOpacity`
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 8px 0;
-`;
-export const ButtonIcon = styled.View``;
-export const ButtonText = styled.Text`
-  font-size: ${({theme})=>theme.FONT_SIZE.SM}px;
-  font-weight: 700;
-  text-align: center;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 16px;
+  text-align: left;
   color: ${({theme})=>theme.COLORS.BLUE};
 `;
 
-export const Container = styled.View`
-  flex: 1;
-  align-items: center;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: 100%;
-  padding: 16px;
-  gap: 16px;
-  background-color: ${({theme})=>theme.COLORS.WHITE};
+export const Data = styled.Text`
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 16px;
+  text-align: left;
+  color: ${({theme})=>theme.COLORS.BLACK};
 `;
 
-export const ContentContainer = styled.View`
-  width: 100%;
-  gap: 16px;
+export const Company = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 16px;
+  text-align: left;
+  color: ${({theme})=>theme.COLORS.BLACK};
 `;
 
-
-
+export const OpenButton = styled.View`
+    border-radius: 0 16px 16px 0;
+    padding: 0 16px;
+    background-color: ${({theme})=>theme.COLORS.GRAY_01};
+    justify-content: center;
+    height: 100%;
+`;
