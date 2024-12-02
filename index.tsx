2c1d6b0eac5c13b491a498e236ee8a0b8809bece
robertcerq23 <robertpaixaosoares@gmail.com>
2024-12-01 22:57:24 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,50 +1,33 @@
-import React from 'react';
-import { Feather } from '@expo/vector-icons';
-import { 
-    Wrapper,
-    Container, 
-    Header, 
-    HeaderButtonContainer, 
-    ButtonIcon, 
-    ButtonText,
-    ContentContainer,
-    Title,
-    Description
-} from '../Details/styles';
+import { Image } from 'react-native';
+import { Wrapper,Container, Form, TextContainer, TextBlack, TextLink, TextLinkContainer } from './styles';
+import BGTop from '../../assets/BGTop.png';
 import Logo from '../../components/Logo';
-import theme from '../../theme';
+import Input from '../../components/Input';
 import { Button } from '../../components/Button';
 
-
-export default function Details({route, navigation }) {
-
-    const {id} = route.params;
-
+export default function FormScreen({navigation}) {
     return (
         <Wrapper>
-            <Header>
-                <HeaderButtonContainer onPress={() => navigation.goBack()}>
-                    <ButtonIcon>
-                        <Feather size={16} name="chevron-left" color={theme.COLORS.BLUE} />
-                    </ButtonIcon>
-                    <ButtonText>
-                        Voltar
-                    </ButtonText>
-                </HeaderButtonContainer>
-                <Logo />
-            </Header>
+            <Image source={BGTop} />
 
             <Container>
-                <ContentContainer>
-                    <Title>{JSON.stringify(id)}</Title>
-                    <Description>Com este id é possível ir no endpoint da API buscar o restante da informação.</Description>
-                </ContentContainer>
 
-                <Button 
-                    title="Entrar em contato" 
-                    noSpacing={true} 
-                    variant='primary'
-                    />
+                <Form>
+                    <Logo />
+                    <Input label='Nome' placeholder='digite seu nome'/>
+                    <Input label='E-mail' placeholder='digite seu e-mail'/>
+                    <Input label='Senha' placeholder='digite sua senha'/>
+                    <Button title="Entrar" noSpacing={true} variant='primary'/>
+                    <TextContainer>
+                        <TextBlack>Já tem uma conta?</TextBlack>
+                        <TextLinkContainer onPress={() => navigation.navigate('Login')}>
+                            <TextLink>
+                                    Faça seu login.
+                            </TextLink>
+                        </TextLinkContainer>
+                    </TextContainer>
+                </Form>
+
             </Container>
         </Wrapper>
     );
--- styles.ts
+++ styles.ts
@@ -1,59 +1,47 @@
 import { styled } from 'styled-components/native';
 
-export const Wrapper = styled.SafeAreaView`
+export const Wrapper = styled.View`
   flex: 1;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
-`;
-
-export const Header = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  width: 90%;
-  padding: 16px 0;
-  border-bottom-width: 1px;
-  border-bottom-style: solid;
-  border-bottom-color: ${({theme})=>theme.COLORS.GREEN};
-`;
-export const HeaderButtonContainer = styled.TouchableOpacity`
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 8px 0;
-`;
-export const ButtonIcon = styled.View``;
-export const ButtonText = styled.Text`
-  font-size: ${({theme})=>theme.FONT_SIZE.SM}px;
-  font-weight: 700;
-  text-align: center;
-  color: ${({theme})=>theme.COLORS.BLUE};
 `;
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  flex-direction: column;
-  justify-content: flex-start;
   width: 100%;
   padding: 16px;
   gap: 16px;
   background-color: ${({theme})=>theme.COLORS.WHITE};
 `;
 
-export const ContentContainer = styled.View`
-  width: 100%;
+export const Form = styled.View`
+  flex: 1;
   gap: 16px;
+  width: 100%;
+  align-items: center;
 `;
 
-export const Title = styled.Text`
-  font-size: ${({theme})=>theme.FONT_SIZE.LG}px;
+export const TextContainer = styled.View`
+  flex-direction: row;
+  gap: 8px;
+  width: max-content;
+`;
+
+export const TextBlack = styled.Text`
+  font-size: ${({theme})=>theme.FONT_SIZE.SM}px;
   font-weight: 700;
   text-align: center;
   color: ${({theme})=>theme.COLORS.BLACK};
 `;
 
-export const Description = styled.Text`
+export const TextLinkContainer = styled.TouchableOpacity`
+  width: auto;
+`;
+
+export const TextLink = styled.Text`
   font-size: ${({theme})=>theme.FONT_SIZE.SM}px;
-  font-weight: 400;
-  text-align: left;
-  color: ${({theme})=>theme.COLORS.BLACK};
+  font-weight: 700;
+  text-align: center;
+  color: ${({theme})=>theme.COLORS.GRAY_03};
 `;
